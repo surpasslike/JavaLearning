f13004ac177d55dba6389d42f52b1f89cf5331a7
like <like@xtourtec.com>
2024-04-28 14:37:15 +0800

    use Thread in MyCallable

--- Thread/src/com/itheima/Callable/Demo.java
+++ Thread/src/com/itheima/Callable/Demo.java
@@ -1,30 +1,21 @@
 package com.itheima.Callable;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
-
+/*创建了一个 FutureTask，然后将其作为一个 Runnable 传递给 Thread 对象。然后，你手动启动这个 Thread*/
 public class Demo {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        // 创建Callable对象
         MyCallable mc = new MyCallable();
 
-        // 创建FutureTask
         FutureTask<String> ft = new FutureTask<>(mc);
+        Thread t1 = new Thread(ft);
 
-        // 创建一个线程池
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+        // 先启动线程
+        t1.start();
 
-        // 使用线程池执行FutureTask
-        executor.execute(ft);
-
-        // 获取执行结果，这将阻塞主线程直到任务完成
+        // 然后获取结果，这将阻塞主线程直到线程t1的计算完成
         String s = ft.get();
 
         System.out.println(s);
-
-        // 关闭线程池
-        executor.shutdown();
     }
 }
